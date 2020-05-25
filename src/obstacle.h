013cd6fcddf0306fb93abe012dd8e17dd5ec0f5b
oystein-PC <olasson>
2020-05-25 10:37:07 +0200

    Added obstacle.h

---
+++ src/obstacle.h
@@ -0,0 +1,13 @@
+#ifndef OBSTACLE_H_
+#define OBSTACLE_H_
+
+// Simple struct for obstacle detection
+
+struct Obstacle
+{
+    double relative_distance;
+    double current_velocity;
+
+};
+
+#endif // OBSTACLE_H_
